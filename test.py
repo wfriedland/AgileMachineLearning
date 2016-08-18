d13b3478950e4fac21759b07552aeb967fc1537b
warrenfriedland <wwfriedland@yahoo.com>
2016-08-18 09:37:13 -0700

    Test KMeans aglorithm on iris dataset. Plot results using pyplot.scatter

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from tests.infrastructure import *
 from tests.naive_bayes import *
 from tests.test_trees import *
 from tests.neural_nets import *
+from tests.test_KMeans_wef import *
 
 if __name__ == '__main__':
     main()
